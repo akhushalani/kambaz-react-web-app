fdb40ffa9f9dd31c3ab740e166bde0b3ee1237b3
Abhinav Khushalani <ask9908@me.com>
2025-07-04 19:53:17 -0400

    Final submission details

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,6 +7,7 @@ export default function TOC() {
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
       <li><Link to="/Kambaz">Kambaz</Link></li>
+      <li><a href="https://github.com/akhushalani/kambaz-react-web-app" id="wd-github">My GitHub</a></li>
     </ul>
   );
 }
