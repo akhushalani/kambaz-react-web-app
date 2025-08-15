d3f53220ffbca2c7b275f8059cba3406246277ad
Abhinav Khushalani <ask9908@me.com>
2025-08-15 14:47:20 -0400

    Updated links

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -34,10 +34,10 @@ export default function TOC() {
       })}
 
       <Nav.Item>
-        <Nav.Link href="https://github.com/akhushalani/kambaz-react-web-app">React GitHub</Nav.Link>
+        <Nav.Link href="https://github.com/akhushalani/kambaz-react-web-app/tree/quizzes">React GitHub</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://github.com/akhushalani/kambaz-node-server-app">Server GitHub</Nav.Link>
+        <Nav.Link href="https://github.com/akhushalani/kambaz-node-server-app/tree/quizzes">Server GitHub</Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link href="https://kambaz-node-server-app-hh98.onrender.com">Server Root</Nav.Link>
